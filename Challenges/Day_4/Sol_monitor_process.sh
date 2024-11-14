eb406eab44100dc6dedfe2c02a5a02465cea5f22
Muhammad Hanzala Ali <alihanzla166@gmail.com>
2024-11-14 23:15:22 +0500

    Update Sol_monitor_process.sh

--- Challenges/Day_4/Sol_monitor_process.sh
+++ Challenges/Day_4/Sol_monitor_process.sh
@@ -1,48 +1,61 @@
 #!/bin/bash
 
-# Function to check if the specified process is running
-is_process_running() {
-    if pgrep -x "$1" >/dev/null; then
-        return 0
-    else
-        return 1
-    fi
+<<usage
+Usage: ./monitor-proc.sh <process-name>
+usage
+
+# specify the target process to monitor
+process_name=$1
+
+# Process Existence Check:
+check_process() {
+        if pgrep -x "$process_name" > /dev/null; then
+                echo "Process '$process_name' is currently running."
+                return 0
+        else
+                echo "Process '$process_name' is not running."
+                return 1
+        fi
 }
 
-# Function to restart the process using systemctl
+# Restart the process
 restart_process() {
-    local process_name="$1"
-    echo "Process $process_name is not running. Attempting to restart..."
+        echo "Process $process_name is not running. Attempting to restart..."
 
-    # Check if the user has the privilege to restart the process
-    if sudo systemctl restart "$process_name"; then
-        echo "Process $process_name restarted successfully."
-    else
-        echo "Failed to restart $process_name. Please check the process manually."
-    fi
+        # Check if the process is still not running before trying to restart
+        if ! pgrep -x "$process_name" > /dev/null; then
+                if sudo systemctl restart "$process_name"; then
+                        echo "Process $process_name started successfully."
+                else
+                        echo "Failed to restart $process_name. Please check the process manually."
+                        exit 1
+                fi
+        else
+                echo "Process $process_name is already running."
+        fi
 }
+
 
 # Check if a process name is provided as an argument
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 <process_name>"
-    exit 1
+        echo "Usage: $0 <process-name>"
+        exit 1
 fi
 
-process_name="$1"
 max_attempts=3
-attempt=1
+attempts=0
 
 # Loop to check and restart the process
-while [ $attempt -le $max_attempts ]; do
-    if is_process_running "$process_name"; then
-        echo "Process $process_name is running."
-    else
-        restart_process "$process_name"
-    fi
+while [ $attempts -lt $max_attempts ]; do
 
-    attempt=$((attempt + 1))
-    sleep 5  # Wait for 5 seconds before the next check
+        if check_process "$process_name"; then
+                echo "Process '$process_name' is running."
+        else
+                restart_process "$process_name"
+        fi
+
+        attempts=$((attempts + 1))
+        sleep 5 # wait for 5 seconds
 done
 
 echo "Maximum restart attempts reached. Please check the process manually."
-
